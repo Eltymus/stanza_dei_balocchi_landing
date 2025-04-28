57e30aca7152ad1e4732d262f331c390813792f4
Lorenzo Tripodi <lorenzo.tripodi98@gmail.com>
2025-04-28 10:01:39 +0200

    Gallery refactor

---
+++ components/AppCarousel.vue
@@ -0,0 +1,79 @@
+<template>
+  <Carousel v-bind="galleryConfig" v-model="currentSlide">
+    <Slide v-for="image in images" :key="image.id">
+      <img
+        :src="image.url"
+        alt="Immagine eventi dei ragazzi"
+        class="rounded-3xl m-4 p-2 border-2 bg-white lg:rotate-3"
+      />
+    </Slide>
+  </Carousel>
+
+  <Carousel v-bind="thumbnailsConfig" v-model="currentSlide">
+    <Slide v-for="image in images" :key="image.id">
+      <template #default="{ currentIndex, isActive }">
+        <div
+          :class="['thumbnail', { 'is-active': isActive }]"
+          @click="slideTo(currentIndex)"
+          class="justify-center items-center cursor-pointer"
+        >
+          <img
+            :src="image.url"
+            alt="Galleria immagini"
+            class="rounded-3xl p-2 border-2 bg-white"
+          />
+        </div>
+      </template>
+    </Slide>
+
+    <template #addons> </template>
+  </Carousel>
+</template>
+
+<script setup>
+import "vue3-carousel/carousel.css";
+import { Carousel, Slide } from "vue3-carousel";
+import { ref } from "vue";
+import { defineProps } from "vue";
+
+const props = defineProps({
+  title: String,
+  subtitle: String,
+  image1: String,
+  image2: String,
+  image3: String,
+});
+
+const currentSlide = ref(0);
+
+const slideTo = (nextSlide) => (currentSlide.value = nextSlide);
+
+const galleryConfig = {
+  itemsToShow: 1,
+  wrapAround: false,
+  slideEffect: "fade",
+  mouseDrag: false,
+  touchDrag: false,
+};
+
+const thumbnailsConfig = {
+  itemsToShow: 3,
+  touchDrag: false,
+  gap: 13,
+};
+
+const images = [
+  {
+    id: 1,
+    url: props.image1,
+  },
+  {
+    id: 2,
+    url: props.image2,
+  },
+  {
+    id: 3,
+    url: props.image3,
+  },
+];
+</script>
--- components/AppGallery.vue
+++ components/AppGallery.vue
@@ -12,43 +12,12 @@
     </div>
 
     <div class="flex flex-col md:w-1/2 sm:w-fit m-4">
-      <Carousel v-bind="galleryConfig" v-model="currentSlide">
-        <Slide v-for="image in images" :key="image.id">
-          <img
-            :src="image.url"
-            alt="Immagine eventi dei ragazzi"
-            class="rounded-3xl m-4 p-2 border-2 bg-white lg:rotate-3"
-          />
-        </Slide>
-      </Carousel>
-
-      <Carousel v-bind="thumbnailsConfig" v-model="currentSlide">
-        <Slide v-for="image in images" :key="image.id">
-          <template #default="{ currentIndex, isActive }">
-            <div
-              :class="['thumbnail', { 'is-active': isActive }]"
-              @click="slideTo(currentIndex)"
-              class="justify-center items-center cursor-pointer"
-            >
-              <img
-                :src="image.url"
-                alt="Galleria immagini"
-                class="rounded-3xl p-2 border-2 bg-white"
-              />
-            </div>
-          </template>
-        </Slide>
-
-        <template #addons> </template>
-      </Carousel>
+      <AppCarousel :image1="image1" :image2="image2" :image3="image3" />
     </div>
   </div>
 </template>
 
 <script setup>
-import "vue3-carousel/carousel.css";
-import { Carousel, Slide } from "vue3-carousel";
-import { ref } from "vue";
 import { defineProps } from "vue";
 
 const props = defineProps({
@@ -58,37 +27,4 @@ const props = defineProps({
   image2: String,
   image3: String,
 });
-
-const currentSlide = ref(0);
-
-const slideTo = (nextSlide) => (currentSlide.value = nextSlide);
-
-const galleryConfig = {
-  itemsToShow: 1,
-  wrapAround: false,
-  slideEffect: "fade",
-  mouseDrag: false,
-  touchDrag: false,
-};
-
-const thumbnailsConfig = {
-  itemsToShow: 3,
-  touchDrag: false,
-  gap: 13,
-};
-
-const images = [
-  {
-    id: 1,
-    url: props.image1,
-  },
-  {
-    id: 2,
-    url: props.image2,
-  },
-  {
-    id: 3,
-    url: props.image3,
-  },
-];
 </script>
