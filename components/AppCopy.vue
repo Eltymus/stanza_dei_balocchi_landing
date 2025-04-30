33c92cdd2964c60474de1ca889deafede2eb6c53
Lorenzo Tripodi <lorenzo.tripodi98@gmail.com>
2025-04-30 15:21:06 +0200

    App copy done

--- components/AppCopy.vue
+++ components/AppCopy.vue
@@ -1,13 +1,21 @@
 <template>
-  <button ref="copy" class="p-3 group">
-    <div class="bg-white rounded-2xl shadow-2xl">
-      <div class="w-full p-4 flex flex-row place-items-center">
-        <font-awesome-icon :icon="socialicon" class="text-gray-600 m-4" />
-        <div class="flex flex-col">
-          <h2 class="font-semibold group-hover:invisible absolute">
+  <button ref="copy" class="p-3 group w-full my-2">
+    <div class="bg-white rounded-full shadow-2xl transition-all duration-300">
+      <div class="w-full p-4 flex flex-row items-center">
+        <font-awesome-icon
+          :icon="socialicon"
+          class="text-gray-600 m-4 group-hover:text-blue-600 text-4xl"
+        />
+        <div class="flex flex-col w-full relative">
+          <h2
+            class="font-semibold group-hover:opacity-0 transition-opacity duration-200"
+          >
             {{ description }}
           </h2>
-          <p ref="foo" class="mx-4 invisible group-hover:visible">
+          <p
+            ref="foo"
+            class="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center opacity-0 group-hover:opacity-100 transition-opacity duration-200 text-blue-600 font-semibold"
+          >
             {{ contact }}
           </p>
         </div>
--- components/AppSocialCard.vue
+++ components/AppSocialCard.vue
@@ -1,9 +1,9 @@
 <template>
-  <a :href="sociallink" target="_blank" class="mx-2">
-    <div class="shadow-2xl rounded-full bg-white p-5 w-fit">
+  <a :href="sociallink" target="_blank" class="mx-2 group">
+    <div class="shadow-2xl rounded-full bg-white p-5 m-4">
       <font-awesome-icon
         :icon="socialicon"
-        class="text-gray-600 hover:text-blue-700 duration-200"
+        class="text-gray-600 group-hover:text-blue-700 duration-200 text-4xl"
       />
     </div>
   </a>
