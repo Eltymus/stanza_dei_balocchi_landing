7a3a03c3d6188ac5fda362fddf84344106fcf9b0
Lorenzo Tripodi <lorenzo.tripodi98@gmail.com>
2025-05-07 13:59:03 +0200

    genrating static site

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  ssr: true,
+  nitro: {
+    preset: "static",
+  },
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   pages: false,
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ["@nuxtjs/tailwindcss"],
   postcss: {
     plugins: {
-      'postcss-color-gray': {}
-    }
+      "postcss-color-gray": {},
+    },
   },
-  tailwindcss: { exposeConfig: true }
-})
+  tailwindcss: { exposeConfig: true },
+});
