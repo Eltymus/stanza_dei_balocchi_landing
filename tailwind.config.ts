15157887e7af1539f458f6336c470d5d01727109
Lorenzo Tripodi <lorenzo.tripodi98@gmail.com>
2025-05-13 14:57:31 +0200

    tailwind working on deploy v1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 export default <Partial<Config>>{
+  enabled: process.env.NODE_ENV === "production",
   theme: {},
   content: [],
-}
+};
