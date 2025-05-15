a8d43f113c39ca0a745d678a6783bd97b22c2a45
Lorenzo Tripodi <lorenzo.tripodi98@gmail.com>
2025-05-15 16:14:44 +0200

    correction to tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  content: ["./app.vue"],
+  safelist: [],
+};
+
+export default config;
