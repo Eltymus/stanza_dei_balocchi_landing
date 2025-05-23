9f1a773bff5f1c05fe85c2b63957b659d33b2b27
Lorenzo Tripodi <lorenzo.tripodi98@gmail.com>
2025-05-23 14:07:02 +0200

    2 change to css path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
   },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ["/assets/css/tailwind.css"],
+  css: ["assets/css/tailwind.css"],
   vite: {
     plugins: [tailwindcss()],
   },
