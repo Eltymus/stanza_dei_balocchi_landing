2b4a8c88c466853b9d0dec4a9724f52f2628715d
Lorenzo Tripodi <lorenzo.tripodi98@gmail.com>
2025-05-23 14:03:38 +0200

    change to css path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite"; // HERE
 
 export default defineNuxtConfig({
   ssr: false,
+  pages: false,
   nitro: {
     preset: "static",
   },
@@ -12,7 +13,7 @@ export default defineNuxtConfig({
   },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ["~/assets/css/tailwind.css"], // HERE
+  css: ["/assets/css/tailwind.css"],
   vite: {
     plugins: [tailwindcss()],
   },
