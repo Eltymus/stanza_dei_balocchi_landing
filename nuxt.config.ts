7c4386f8c187ffb2ac0214fff20f765ef4a7a44b
Lorenzo Tripodi <lorenzo.tripodi98@gmail.com>
2025-05-28 22:39:05 +0200

    fix path in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
     preset: "static",
   },
   app: {
-    baseURL: "/",
+    baseURL: "/dist",
   },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
