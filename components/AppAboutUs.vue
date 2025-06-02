a51f1a299087f7b334a4e7d782def4e7a87d0ea1
Lorenzo Tripodi <lorenzo.tripodi98@gmail.com>
2025-06-02 17:04:49 +0200

    feat: add about us component

---
+++ components/AppAboutUs.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="max-w-7xl mx-auto">
+    <div :style="{ backgroundImage: `url(${imageBg})` }" class="bg-cover">
+      <div class="backdrop-blur-sm">
+        <div class="bg-white p-5 w-7/12 mx-auto text-center">
+          <p>Aiuto</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["imageBg"],
+};
+</script>
